e2e209735c66cf82aa17a5e96fd2c02a3831f3b0
Abhijit <akarmakar846@gmail.com>
2023-07-27 23:02:05 +0530

    Changes 10

--- api/index.js
+++ api/index.js
@@ -1,35 +1,16 @@
-// index.js
-const express = require('express');
-const mongoose = require('mongoose');
-const { MONGOURI, MONGOPASSWORD } = require('../keys');
-const app = express();
-app.use(express.json());
-const PORT = 4000;
+const app = require('express')();
+const { v4 } = require('uuid');
 
-const MONGO_URL = MONGOURI;
-const MONGO_PASSWORD = MONGOPASSWORD;
-
-// connect with mongodb
-const DB = MONGO_URL.replace("<password>", MONGO_PASSWORD);
-
-mongoose.connect(DB)
-  .then(() => {
-    console.log("Connction successful with DB");
-  }).catch((err) => {
-    console.log("Something went wrong in connection");
-  });
-
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `);
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳');
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
-app.get('/about', (req, res) => {
-  res.send('This is my about route..... ');
-});
-
-// Export the Express API
 module.exports = app;
