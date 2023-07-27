b1aa7bf0fbe578e8ab5b8cb77491100ad60bcd57
Abhijit <akarmakar846@gmail.com>
2023-07-27 19:43:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// Add Express
+const express = require("express");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Initialize Express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// configure dotenv
+dotenv.config({ path: './config.env' });
+
+// static variable
+const MONGO_URL = process.env.DATABASE_URI;
+const MONGO_PASSWORD = process.env.DATABASE_PASSWORD;
+const PORT = process.env.PORT || 5000;
+
+// connect with mongodb
+const DB = MONGO_URL.replace("<password>", MONGO_PASSWORD);
+
+mongoose.connect(DB)
+  .then(() => {
+    console.log("Connction successful with DB");
+  }).catch((err) => {
+    console.log("Something went wrong in connection");
+  });
+
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Express app on vercel." });
+});
+
+// Initialize server
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}.`);
+});
+
+// Export the Express API
+module.exports = app;
